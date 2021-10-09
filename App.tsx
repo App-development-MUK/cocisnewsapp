33fc8bd75f9b7135c2ae9c6538b04013e9ad01d0
katznicho <katznicho@gmail.com>
2021-10-09 15:38:23 +0300

    set up template

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello Cocis</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
